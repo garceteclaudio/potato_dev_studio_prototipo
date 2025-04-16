d30833581d01581e7fedbcbe3ae6f89ad40ed538
JoseMurilloTS <165093208+JoseMurilloTS@users.noreply.github.com>
2025-04-16 20:36:31 -0300

    creacion del menu
    - Creado el prototipo del menú.
    - Añadidos archivos de prueba para el menú.

--- Assets/Scripts/Mapa/EfectoHover.cs
+++ Assets/Scripts/Mapa/EfectoHover.cs
@@ -4,11 +4,16 @@ using UnityEngine.EventSystems;
 public class EfectoHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject imagenHover;
+    public AudioSource audioSource;
+    public AudioClip hoverSound;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (imagenHover != null)
             imagenHover.SetActive(true);
+
+        if (audioSource != null && hoverSound != null)
+            audioSource.PlayOneShot(hoverSound);
     }
 
     public void OnPointerExit(PointerEventData eventData)
