728164bd77d1e6aca6fc8d94a0bec4ff22245ce6
Rubenxdf <165188410+Rubenxdf@users.noreply.github.com>
2025-04-22 15:16:44 -0300

    mejora en el script de  2minijuego2"
    agregue un script para la comida "mala" que resta vidas o intentos , una vez que pierdas las vidas carga un mensaje por consola
    -mecanicas para perder vidas y se puede visualizar en el canvas

--- Assets/minijuego2/scripts/GameManager.cs
+++ Assets/minijuego2/scripts/GameManager.cs
@@ -5,6 +5,10 @@ public class GameManager : MonoBehaviour
    public static GameManager Instance { get; private set; }
     public int PuntosTotales { get { return puntosTotales; } } 
   private  int puntosTotales;
+    
+    //public int vidas = 5;
+    public int Vidas { get; private set; } = 5;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
@@ -22,5 +26,24 @@ public class GameManager : MonoBehaviour
     {
         puntosTotales += puntosASumar;
         Debug.Log(puntosTotales);
+
+        if (puntosTotales >= 20)
+        {
+            Debug.Log("¡Ganaste!");
+          //  SceneManager.LoadScene("Win"); // Asegurate que la escena esté añadida en Build Settings
+        }
     }
+    
+    public void RestarVida()
+    {
+        Vidas--;
+        Debug.Log("Vidas restantes: " + Vidas);
+
+        if (Vidas <= 0)
+        {
+            Debug.Log("¡Juego terminado!");
+            
+        }
+    }
+   
 }
